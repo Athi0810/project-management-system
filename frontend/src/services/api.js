1ed398562fd79f6032118760d3d3a6c23758a6ab
Aadhi <athithyan083.gmail.com>
2026-09-10 14:21:33 +0530

    Fix deployed API URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://project-management-system-m4g4.onrender.com"
+    baseURL:  "https://project-management-system-m4g4.onrender.com/api"
 });
 
 api.interceptors.request.use((config) => {
